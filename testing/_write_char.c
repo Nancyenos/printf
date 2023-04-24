8d9a39826416db50c13b1e1b0897c5386e54283f
yemsal4media <yemsalforlove2012@gmail.com>
2023-04-24 22:01:07 +0000

    removing Our_printf

---
+++ testing/_write_char.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _write_char - This function writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return:	1 On success.
+ *			-1 is returned on error.
+ */
+ 
+int _write_char(char c)
+{
+	return (write(1, &c, 1));
+}
